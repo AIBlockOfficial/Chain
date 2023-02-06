b1ef63247783658b44b68e1d06e3fd40a754fce1
Riccardo <riccardo.allegrone@pm.me>
2023-02-06 11:36:19 +0100

    OP_2DUP implementation

--- src/script/interface_ops.rs
+++ src/script/interface_ops.rs
@@ -35,6 +35,25 @@ pub fn op_2drop(current_stack: &mut Vec<StackEntry>) -> bool {
     true
 }
 
+/// Handles the execution for the double duplicate opcode. Returns a bool.
+///
+/// ### Arguments
+///
+/// * `current_stack`  - mutable reference to the current stack
+pub fn op_2dup(current_stack: &mut Vec<StackEntry>) -> bool {
+    trace!("OP_2DUP: duplicating last two entries of the stack");
+    let len = current_stack.len();
+    if len < 2 {
+        error!("Not enough elements on the stack");
+        return false;
+    }
+    let dup1 = current_stack[len - 2].clone();
+    let dup2 = current_stack[len - 1].clone();
+    current_stack.push(dup1);
+    current_stack.push(dup2);
+    true
+}
+
 /// Handles the execution for the drop opcode. Returns a bool.
 ///
 /// ### Arguments
@@ -63,8 +82,8 @@ pub fn op_dup(current_stack: &mut Vec<StackEntry>) -> bool {
         error!("Stack is empty");
         return false;
     }
-    let last_entry = current_stack[len - 1].clone();
-    current_stack.push(last_entry);
+    let dup = current_stack[len - 1].clone();
+    current_stack.push(dup);
     true
 }
 
